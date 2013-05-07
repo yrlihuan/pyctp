803c2257374a35de6fff3e55ddde21e212b0507e
Huan Li <yrlihuan@gmail.com>
2013-05-07 09:20:06 +0800

    config.sh include ctp_future bin dir

--- config.sh
+++ config.sh
@@ -1,2 +1,2 @@
 export PYCTPPATH="/home/huan/workspace/pyctp"
-export PYTHONPATH="$PYTHONPATH:$PYCTPPATH/bin/linux64_sse"
+export PYTHONPATH="$PYTHONPATH:$PYCTPPATH/bin/linux64_sse:$PYTHONPATH/bin/linux64"
